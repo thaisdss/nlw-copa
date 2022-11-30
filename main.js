4ef46ab5a45b2b9b6e12acbd5d9e41af1a22e725
Thaís Silva <thaisdss.silva@gmail.com>
2022-11-30 15:46:02 -0300

    update nlw-copa

--- main.js
+++ main.js
@@ -8,9 +8,9 @@ function createGame(player1, hour, player2) {
   `
 }
 
-let delay = -0.4;
+let delay = -0.4
 function createCard(date, day, games) {
-  delay = delay + 0.4;
+  delay = delay + 0.4
   return `
     <div class="card" style="animation-delay: ${delay}s">
       <h2>${date} <span>${day}</span></h2>
@@ -22,29 +22,30 @@ function createCard(date, day, games) {
 }
 
 document.querySelector("#cards").innerHTML =
-    createCard(
-      "01/12",
-      "quinta",
-      createGame("japan", "16:00", "spain") +
-        createGame("costa-rica", "16:00", "germany")
-    ) 
-    +
-    createCard(
-      "02/12",
-      "sexta",
-      createGame("brazil", "16:00", "cameroon") +
-        createGame("serbia", "16:00", "switzerland")
-    );
+  createCard(
+    "01/12",
+    "quinta",
+    createGame("japan", "16:00", "spain") +
+      createGame("costa-rica", "16:00", "germany")
+  ) +
+  createCard(
+    "02/12",
+    "sexta",
+    createGame("brazil", "16:00", "cameroon") +
+      createGame("serbia", "16:00", "switzerland")
+  )
 
-const modeBlue = () => {
-  document.querySelector("body").classList.remove("green")
-  document.querySelector("body").classList.toggle("blue")
+const removeClass = (elementId) => {
+  document.querySelector("body").classList.remove(elementId)
 }
 
-const modeGreen = () => {
-  document.querySelector("body").classList.remove("blue")
-  document.querySelector("body").classList.toggle("green")
+const modeBg = (event) => {
+  const elementId = event.target.id
+
+  elementId === "blue" ? removeClass("green") : removeClass("blue")
+
+  document.querySelector("body").classList.toggle(elementId)
 }
 
-document.querySelector("#icon-bg-blue").addEventListener("click", modeBlue);
-document.querySelector("#icon-bg-green").addEventListener("click", modeGreen);
+document.querySelector("#blue").addEventListener("click", modeBg)
+document.querySelector("#green").addEventListener("click", modeBg)
